acaa0a6cd3119f9f71fdf3e06511593387f72f7f
Budai Péter <budai.ptr2@gmail.com>
2017-04-30 19:20:02 +0200

    fix

---
+++ cinema/src/main/java/com/cinema/service/impl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.cinema.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cinema.model.CustomUserDetails;
+import com.cinema.model.User;
+import com.cinema.repository.UserRepository;
+import com.cinema.repository.UserRolesRepository;
+import com.cinema.service.CustomUserDetailsService;
+
+@Service("customUserDetailsService")
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+	private final UserRepository userRepository;
+	private final UserRolesRepository userRolesRepository;
+
+	@Autowired
+	public CustomUserDetailsServiceImpl(UserRepository userRepository, UserRolesRepository userRolesRepository) {
+		this.userRepository = userRepository;
+		this.userRolesRepository = userRolesRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findUserByName(username);
+		if (null == user) {
+			throw new UsernameNotFoundException("No user present with username: " + username);
+		} else {
+			List<String> userRoles = userRolesRepository.findRoleByUserName(username);
+			return new CustomUserDetails(user, userRoles);
+		}
+	}
+
+	
+}
